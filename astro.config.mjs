942bad7f0bd1724d9e861905c348008a0d07003d
Salvador Vasquez <salvador97vasqz@gmail.com>
2025-09-17 21:07:17 -0600

    Add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: 'https://salvadorvasqz.github.io',
   base: "/reactune-demo/",
   integrations: [react()],
   vite: {
